4ced9885bf4966faf7093273e5afbc31fc8c3a3c
Erik Opinaldo <erikopinaldo@gmail.com>
2022-10-18 19:38:14 -0400

    copy all files to root folder

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'erikopinaldo'"; //letter subject
+	$to = 'erikopinaldo@gmail.com'; // Enter Your E-mail
+	$from = 'admin@you-site-name.com'; // Admin e-mail
+	$fromName = 'erikopinaldo'; // Your company name
+	$charset = 'UTF-8';
+?>
