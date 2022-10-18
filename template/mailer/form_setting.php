d1c2e61ca7a11d2a275970dead42715681ecb80c
Erik Opinaldo <erikopinaldo@gmail.com>
2022-10-18 19:42:06 -0400

    delete nested content folder

--- template/mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'erikopinaldo'"; //letter subject
-	$to = 'erikopinaldo@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'erikopinaldo'; // Your company name
-	$charset = 'UTF-8';
-?>
